25d4eece4066941281d9d74812109efd8974d671
yasmimcosm <yasmimcosm@gmail.com>
2026-01-25 16:21:36 -0300

    aprendendo variáveis

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Olá mundo!')
+idade = 18
+console.log('Minha idade atual:', idade)
+
+// var || let || const
+
+//variável global
+idade = 18
+
+//var
+var idade = 39
+
+//let
+let idade = 10
+
+//const
+const idade = 30
