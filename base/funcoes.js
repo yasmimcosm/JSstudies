69ba35837b4b61a3e6bd4ae5552c995fa0d9cfb5
yasmimcosm <yasmimcosm@gmail.com>
2026-01-25 19:32:38 -0300

    funcoes

---
+++ base/funcoes.js
@@ -0,0 +1,15 @@
+function saudacao(nome){
+    console.log('Olá,', nome,'!')
+}
+
+saudacao('Mimi')
+
+const oiie = (nome) => {
+    console.log('Oiie', nome)
+}
+
+oiie('Ivan')
+
+const mensagemOdio = nome => console.log('Eu odeio... Eu odeio... Eu odeio você', nome)
+
+mensagemOdio("Maria Joaquina")
