febfcfd00afbe18a9c6b1aa07025e1b77fbfd8e9
yasmimcosm <yasmimcosm@gmail.com>
2026-01-25 17:16:00 -0300

    exercícios

---
+++ exercicios.js
@@ -0,0 +1,13 @@
+console.log('Bem-vindo(a)!')
+
+let valorCompra = 17.50
+let valorPago = 20
+
+console.log('O troco ficou', valorPago - valorCompra)
+
+//Validação de senha
+let senha = 'mimizinha'
+let senhaDigitada = 'mimizinha'
+const saoIguais = senha == senhaDigitada
+
+console.log('As senhas são iguais?', saoIguais)
--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 console.log('Olá mundo!')
-idade = 18
-console.log('Minha idade atual:', idade)
 
-// var || let || const
+var num = 39 //var
 
-//variável global
-idade = 18
+let valor = 10 //let
 
-//var
-var idade = 39
+const idades = 30 //const
 
-//let
-let idade = 10
+console.log(typeof idade) //diz o tipo da variável
 
-//const
-const idade = 30
+let anoAtual = 2026
+
+let anoNascimento = 2007
+
+console.log('Você tem ', anoAtual - anoNascimento, ' anos')
+
+//Symbol em JavaScript é um tipo de dado primitivo usado justamente para criar valores únicos, mesmo que tenham a mesma descrição.
+const user = {
+name: "Yasmim"
+};
+
+const id = Symbol("id");
+
+user[id] = 123;
+
+console.log(user);
+// { name: 'Yasmim', [Symbol(id)]: 123 }
+
+
+const saoIguais = idadeVinny == minhaIdade;
+console.log('São iguais? ', saoIguais);
+
+const comparacaoEstrita = minhaIdade === idadeVinny;
+console.log('São estritamente iguais?', comparacaoEstrita);
+
+const comparacaoEstrita2 = minhaIdade !== idadeVinny;
+console.log('São estritamente diferentes?', comparacaoEstrita);
+
+
+
+const idade = 15;
+const maiorDeIdade = idade >= 18;
+const possuiCNH = false;
+const podeDirigir = maiorDeIdade && possuiCNH; // AND
+const podeViajarSozinha = maiorDeIdade || possuiCNH; // OR
+const precisaDeAcompanhante = !maiorDeIdade; // NOT
+console.log('Pode dirigir?', podeDirigir);
