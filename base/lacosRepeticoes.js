1d714eac82ccc4522a4cc4da64e4e883039bb5c3
yasmimcosm <yasmimcosm@gmail.com>
2026-02-02 17:06:04 -0300

    Atualizando o for

--- base/lacosRepeticoes.js
+++ base/lacosRepeticoes.js
@@ -8,7 +8,29 @@
         } else {
             console.log('O número', i, 'é ímpar')
         }
+    }   
+
+    for (let contador = 1; contador <= 50; contador++) {
+        const numero = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+        if (numero === 15) {
+            console.log(`${numero} em ${contador} tentativas`);
+            break;
+        }
     }
+
+    //O continue pula para a próxima iteração e desconsidera o restante do bloco atual.
+    let contador = 0;
+
+    for (let i = 1; i <= 20; i++) {
+        const numero = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+        if (numero % 5 === 0) {
+            continue;
+        }
+        contador++;
+    }
+
+
+    console.log('contador', contador);
 
     const palavra = 'Yasmim'
 
